5df673a281d5a7128aebb430c4741c9e9898bc81
Manuel <jsuarez1804@alumno.ipn.mx>
2022-09-14 02:56:34 -0500

    tokenization and lemmatization

--- p1.py
+++ p1.py
@@ -1,7 +1,13 @@
 import stanza
-
 nlp = stanza.Pipeline(lang='es', processors='tokenize,mwt,pos,lemma')
-doc = nlp('Los trabajadores trabajan mucho.')
 
-for sentence in doc.sentences:
-    print(*[f'word: {word.text + " "}\tlemma: {word.lemma}' for sent in doc.sentences for word in sent.words], sep='\n')
+texto = """Ocupando sus lugares en el cohete, Neil Armstrong, Edwin Aldrin y 
+Michael Collins compartían un vacío en el estómago. Esto era para lo que habían 
+entrenado. Estaban listos. Pero la sombra del incendio del Apolo 1 acechaba en 
+algún lugar remoto de sus mentes. Era inevitable."""
+print(texto)
+doc = nlp(texto)
+
+print(*[f'token: {token.text}' for sentence in doc.sentences for token in sentence.tokens], sep='\n')
+
+print(*[f'p: {tok.text+" "}\tlemma: {tok.lemma}' for sent in doc.sentences for tok in sent.words], sep='\n')
